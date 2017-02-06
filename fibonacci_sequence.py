8abe32c3f1f38539a18a431987c8d8097912cd8a
Bartek Dalak <bd370768@students.mimuw.edu.pl>
2017-02-06 17:29:29 +0100

    generating fibonacci sequence

---
+++ fibonacci_sequence.py
@@ -0,0 +1,15 @@
+# generate sequence of fibonacii numbers to nth number
+# we assume that F0 = 0
+
+def fib_sequence(n):
+    current = 0
+    next = 1
+    seq = [current]
+    for _ in range(n):
+        current, next = next, current + next
+        seq.append(current)
+    return seq
+
+n = int(input("Type the index of last fibonnaci number:"))
+fib_numbers = fib_sequence(n)
+print (fib_numbers)
